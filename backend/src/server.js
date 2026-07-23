790cdae2290769ff49dd1f1774d812499992406a
rushikeshk24 <rushikeshkarlekar14@gmail.com>
2026-07-24 01:06:15 +0530

    Update project

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import dotenv from 'dotenv';
+import dns from "node:dns";
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 dotenv.config({ path: join(__dirname, '..', '.env') });
